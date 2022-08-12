4b1d947cf84b8985b0b30a49794b48a6d74ad9c0
Leonardo Miguel Aguado Diaz <leonardomiguelaguadodiaz@gmail.com>
2022-08-11 20:15:40 -0500

    init commit

---
+++ hulk_store_backend/src/main/java/com/todo1/hulk_store_backend/HulkStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.todo1.hulk_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulkStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulkStoreBackendApplication.class, args);
+	}
+
+}
